295c742150a24bed8bc1af12a81667985d7c6443
Suraj Kale <kalesuraj37193@gmail.com>
2021-06-09 23:28:57 +0530

    Added some basic UIs

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# FoodSupplyChainHackathon
+# HackerearthProject01
+This project is dedicated to providing fresh farm products to customers and help farmers to be 'Atma Nirbhar' by eleminating the middle man from supply chain.
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const app = express();
+
+// setting views
+app.use(expressLayouts);
+app.set('views', path.join(__dirname + '/views'));
+app.set('view engine', 'ejs');
+
+//setting static files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/images', express.static(__dirname + 'public/images'));
+app.use('/js', express.static(__dirname + 'public/js'));
+
+//setting paths
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', { Title: 'Login' });
+});
+
+app.get('/register', (req, res) => {
+    res.render('register', { Title: 'Register' });
+});
+
+app.get('/getCoordinates', (req, res) => {
+    res.render('getCoordinates');
+});
+
+app.get('/geoCoder', (req, res) => {
+    res.render('geoCoder');
+});
+
+const PORT = 3333;
+app.listen(PORT, () => console.log('Listening on 3333'));
