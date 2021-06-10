e316cacdcd8b6256d18f029d3d17e1e613f6423c
Suraj Kale <kalesuraj37193@gmail.com>
2021-06-11 00:52:51 +0530

    Updated userController. Added encryption to password. Created isUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,54 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const User = mongoose.model('Admin')
+const bcrypt = require('bcryptjs');
+const User = mongoose.model('User');
 
-module.exports.registerPost = async(req, res) => {
+module.exports.registerPost = async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
+    const user = {
+        name: req.body.fullName,
+        email: req.body.email,
+        password: hashedPassword,
+        mobileNum: req.body.mobileNum,
+        state: req.body.state,
+        district: req.body.district,
+        landmark: req.body.landMark,
+        flatNum: req.body.flatNumber,
+        pinCode: req.body.pinCode,
+    };
+
+    req.session.userData = user;
+
+    res.redirect("/user/location");
+}
+
+module.exports.saveUser = async (req, res) => {
+    var newUser = new User();
+
+    newUser.name = req.session.userData.name;
+    newUser.email = req.session.userData.email;
+    newUser.password = req.session.userData.password;
+    newUser.mobileNum = req.session.userData.mobileNum;
+    newUser.address.state = req.session.userData.state;
+    newUser.address.district = req.session.userData.district;
+    newUser.address.landmark = req.session.userData.landmark;
+    newUser.address.flatNum = req.session.userData.flatNum;
+    newUser.address.pinCode = req.session.userData.pinCode;
+    newUser.lattitude = req.params.lat;
+    newUser.longitude = req.params.lng;
+
+    req.session = null;
+    await newUser
+        .save()
+        .then(() => {
+            console.log("User Saved");
+        })
+        .catch((err) => {
+            console.error(err);
+            return res.send("Error saving user" + err);
+        });
+
+    res.redirect("/user/login");
 }
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs')
 
 const router = express.Router();
 const path = require("path");
 const app = express();
 
 const User = mongoose.model("User");
+
+const userController = require('../controllers/userController');
+
+const isUser = require('../other/isUser');
 
 router.get("/", (req, res) => {
   if (req.session == null) user = "";
@@ -36,23 +41,7 @@ router.get("/logout", (req, res) => {
 });
 
 //store all the posted data in session
-router.post("/register", async (req, res) => {
-  const user = {
-    name: req.body.fullName,
-    email: req.body.email,
-    password: req.body.password,
-    mobileNum: req.body.mobileNum,
-    state: req.body.state,
-    district: req.body.district,
-    landmark: req.body.landMark,
-    flatNum: req.body.flatNumber,
-    pinCode: req.body.pinCode,
-  };
-
-  req.session.userData = user;
-
-  res.redirect("/user/location");
-});
+router.post("/register", isUser.isUser, userController.registerPost);
 
 //render map to get co-ordinates
 router.get("/location", (req, res) => {
@@ -60,33 +49,6 @@ router.get("/location", (req, res) => {
 });
 
 //get lng and lat and rest of user data from session and store in DB
-router.get("/saveUser/:lng/:lat", async (req, res) => {
-  var newUser = new User();
-
-  newUser.name = req.session.userData.name;
-  newUser.email = req.session.userData.email;
-  newUser.password = req.session.userData.password;
-  newUser.mobileNum = req.session.userData.mobileNum;
-  newUser.address.state = req.session.userData.state;
-  newUser.address.district = req.session.userData.district;
-  newUser.address.landmark = req.session.userData.landmark;
-  newUser.address.flatNum = req.session.userData.flatNum;
-  newUser.address.pinCode = req.session.userData.pinCode;
-  newUser.lattitude = req.params.lat;
-  newUser.longitude = req.params.lng;
-
-  req.session = null;
-  await newUser
-    .save()
-    .then(() => {
-      console.log("User Saved");
-    })
-    .catch((err) => {
-      console.error(err);
-      return res.send("Error saving user" + err);
-    });
-
-  res.redirect("/user/login");
-});
+router.get("/saveUser/:lng/:lat", userController.saveUser);
 
 module.exports = router;
